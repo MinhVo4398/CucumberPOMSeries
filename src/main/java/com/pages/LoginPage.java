b02c9f8fb813c45258b09be07e8249071dc8f051
DELL <minhvo4398@gmail.com>
2022-10-26 12:06:19 +0700

    Initial commit

---
+++ src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,43 @@
+package com.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class LoginPage {
+
+    private WebDriver driver;
+
+    // 1. By Locators
+    private By emailId = By.id("email");
+    private By password = By.id("passwd");
+    private By signInButton = By.id("SubmitLogin");
+    private By forgotPasswordLink = By.xpath("//a[text()='Forgot your password?']");
+
+    //2. Constructor of the page class:
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //3. page actions: feature (behaviour) of the page form of methods:
+    public String getLoginPageTitle() {
+       return driver.getTitle();
+    }
+
+    public boolean isForgotPasswordLinkExist() {
+      return  driver.findElement(forgotPasswordLink).isDisplayed();
+    }
+
+    public void enterUserName(String userName) {
+        driver.findElement(emailId).sendKeys(userName);
+    }
+
+    public void enterPassword(String pwd) {
+        driver.findElement(password).sendKeys(pwd);
+    }
+    public void clickOnLogin() {
+        driver.findElement(signInButton).click();
+    }
+
+}
